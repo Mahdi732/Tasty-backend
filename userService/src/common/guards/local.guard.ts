fd680c831d92acd868d924dc24f503da5947c590
Mahdi732 <ey449751@gmail.com>
2026-01-29 09:36:26 +0100

    add LocalAuthGuard for handling local authentication in GraphQL

---
+++ userService/src/common/guards/local.guard.ts
@@ -0,0 +1,14 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+@Injectable()
+export class LocalAuthGuard extends AuthGuard('local') {
+  getRequest(context: ExecutionContext) {
+    if (context.getType<'graphql'>() === 'graphql') {
+      const ctx = GqlExecutionContext.create(context);
+      return ctx.getContext().req;
+    }
+    return context.switchToHttp().getRequest();
+  }
+}
