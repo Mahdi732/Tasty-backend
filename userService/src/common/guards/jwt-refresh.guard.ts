3a0fd6989bef52e977c7907b48f67ec3711fe050
Mahdi732 <ey449751@gmail.com>
2026-01-29 09:36:18 +0100

    add JwtRefreshGuard for handling JWT refresh token authentication in GraphQL

---
+++ userService/src/common/guards/jwt-refresh.guard.ts
@@ -0,0 +1,14 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+@Injectable()
+export class JwtRefreshGuard extends AuthGuard('jwt-refresh') {
+  getRequest(context: ExecutionContext) {
+    if (context.getType<'graphql'>() === 'graphql') {
+      const ctx = GqlExecutionContext.create(context);
+      return ctx.getContext().req;
+    }
+    return context.switchToHttp().getRequest();
+  }
+}
