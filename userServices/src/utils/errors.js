67e54c5d1b43a406a5b03b58d109c8cc71eccc51
Mahdi732 <ey449751@gmail.com>
2026-01-29 09:41:33 +0100

    remove AppError class from errors.js as part of project restructuring

--- userServices/src/utils/errors.js
+++
@@ -1,8 +0,0 @@
-export class AppError extends Error {
-  constructor(message, statusCode = 500, code = "ERROR", errors = undefined) {
-    super(message);
-    this.statusCode = statusCode;
-    this.code = code;
-    this.errors = errors;
-  }
-}
