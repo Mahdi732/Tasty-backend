b399772fe0264ec9a7901065e1a05ad0510e714e
Mahdi732 <ey449751@gmail.com>
2026-02-23 13:58:27 +0000

    remove cypress.config.js, eslint.config.mjs, and nest-cli.json as part of project restructuring

--- userService/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:3000/api',
-    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
-    supportFile: false,
-  },
-});
