06ef9e277520ae624c92642d07e5b56954c42491
Mahdi732 <ey449751@gmail.com>
2026-02-23 13:58:38 +0000

    remove health.cy.js, schema.prisma, and app.module.ts as part of project restructuring

--- userService/cypress/e2e/health.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Health endpoint', () => {
-  it('returns ok', () => {
-    cy.request('/health').then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.status).to.eq('ok');
-    });
-  });
-});
