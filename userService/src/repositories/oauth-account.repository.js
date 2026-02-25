275d5083eb979e58bb16a55d5cd84f3effee0c46
Mahdi732 <ey449751@gmail.com>
2026-02-25 11:03:23 +0000

    feat: implement OAuthAccountRepository for managing OAuth account data

---
+++ userService/src/repositories/oauth-account.repository.js
@@ -0,0 +1,25 @@
+export class OAuthAccountRepository {
+  constructor(oauthAccountModel) {
+    this.oauthAccountModel = oauthAccountModel;
+  }
+
+  async findByProviderAndProviderUserId(provider, providerUserId) {
+    return this.oauthAccountModel.findOne({ provider, providerUserId });
+  }
+
+  async findByUserAndProvider(userId, provider) {
+    return this.oauthAccountModel.findOne({ userId, provider });
+  }
+
+  async create(payload) {
+    return this.oauthAccountModel.create(payload);
+  }
+
+  async updateLoginMeta(id, updates) {
+    return this.oauthAccountModel.findByIdAndUpdate(id, updates, { new: true });
+  }
+
+  async deleteByUserAndProvider(userId, provider) {
+    return this.oauthAccountModel.findOneAndDelete({ userId, provider });
+  }
+}
