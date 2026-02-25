d767ce1b985099d444f60abdaa0974b4ee6a5836
Mahdi732 <ey449751@gmail.com>
2026-02-25 11:07:02 +0000

    feat: add getClientIp utility for retrieving client IP address

---
+++ userService/src/utils/ip.js
@@ -0,0 +1,7 @@
+export const getClientIp = (req) => {
+  const xForwardedFor = req.headers['x-forwarded-for'];
+  if (xForwardedFor) {
+    return xForwardedFor.split(',')[0].trim();
+  }
+  return req.ip;
+};
