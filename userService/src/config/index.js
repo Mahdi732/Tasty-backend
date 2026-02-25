2546277d0b1774df0f99c8a7b691cf1d28cafe65
Mahdi732 <ey449751@gmail.com>
2026-02-25 10:59:45 +0000

    feat: create configuration module for environment, logging, database, CORS, and security

---
+++ userService/src/config/index.js
@@ -0,0 +1,18 @@
+import { env } from './env.js';
+import { logger, httpLogger } from './logger.js';
+import { connectMongo } from './mongoose.js';
+import { createRedisClient } from './redis.js';
+import { buildCorsOptions } from './cors.js';
+import { buildHelmetOptions } from './helmet.js';
+import { JwksConfig } from './jwks.js';
+
+export {
+  env,
+  logger,
+  httpLogger,
+  connectMongo,
+  createRedisClient,
+  buildCorsOptions,
+  buildHelmetOptions,
+  JwksConfig,
+};
