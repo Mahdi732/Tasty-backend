4087a90bef8e125c5a215eb4601c4eca9049c955
Mahdi732 <ey449751@gmail.com>
2026-02-25 11:00:16 +0000

    feat: add authentication constants for access tokens and OAuth providers

---
+++ userService/src/constants/auth.js
@@ -0,0 +1,12 @@
+export const ACCESS_TOKEN_TYPE = 'access';
+export const REFRESH_TOKEN_TRANSPORT_MODES = Object.freeze({
+  COOKIE: 'cookie',
+  BODY: 'body',
+  BOTH: 'both',
+});
+
+export const OAUTH_PROVIDERS = Object.freeze({
+  GOOGLE: 'google',
+  FACEBOOK: 'facebook',
+  APPLE: 'apple',
+});
