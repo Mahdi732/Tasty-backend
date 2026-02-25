584c28cb1757252032525b20731804356aac29a4
Mahdi732 <ey449751@gmail.com>
2026-02-25 11:01:58 +0000

    feat: add rate limiter middleware for request throttling

---
+++ userService/src/middlewares/rate-limit.middleware.js
@@ -0,0 +1,16 @@
+import rateLimit from 'express-rate-limit';
+import { RedisStore } from 'rate-limit-redis';
+
+export const createRateLimiter = (redisClient, options) =>
+  rateLimit({
+    standardHeaders: true,
+    legacyHeaders: false,
+    ...(redisClient && process.env.NODE_ENV !== 'test'
+      ? {
+          store: new RedisStore({
+            sendCommand: (...args) => redisClient.sendCommand(args),
+          }),
+        }
+      : {}),
+    ...options,
+  });
