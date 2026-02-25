77af10c0c255c67e7b2b04c56ad73f5c40262e26
Mahdi732 <ey449751@gmail.com>
2026-02-25 11:04:10 +0000

    feat: implement JwtSigner class for JWT access token signing

---
+++ userService/src/security/jwt.signer.js
@@ -0,0 +1,21 @@
+import { SignJWT } from 'jose';
+
+export class JwtSigner {
+  constructor({ privateKey, issuer, audience, accessTtlSeconds, activeKid }) {
+    this.privateKey = privateKey;
+    this.issuer = issuer;
+    this.audience = audience;
+    this.accessTtlSeconds = accessTtlSeconds;
+    this.activeKid = activeKid;
+  }
+
+  async signAccessToken(payload) {
+    return new SignJWT(payload)
+      .setProtectedHeader({ alg: 'RS256', kid: this.activeKid, typ: 'JWT' })
+      .setIssuedAt()
+      .setIssuer(this.issuer)
+      .setAudience(this.audience)
+      .setExpirationTime(`${this.accessTtlSeconds}s`)
+      .sign(this.privateKey);
+  }
+}
