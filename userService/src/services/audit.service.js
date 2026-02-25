051982314e0b19c755669ccaaa4743a4ad5013f0
Mahdi732 <ey449751@gmail.com>
2026-02-25 11:05:06 +0000

    feat: implement AuditService class for logging security audit events

---
+++ userService/src/services/audit.service.js
@@ -0,0 +1,9 @@
+export class AuditService {
+  constructor(logger) {
+    this.logger = logger;
+  }
+
+  log(event, payload = {}) {
+    this.logger.info({ event, ...payload }, 'security_audit_event');
+  }
+}
