78d46b7d203034d591416269d89e17de8a2847de
Mahdi732 <ey449751@gmail.com>
2026-02-25 10:59:39 +0000

    feat: add helmet configuration for security headers

---
+++ userService/src/config/helmet.js
@@ -0,0 +1,12 @@
+export const buildHelmetOptions = (isProduction) => ({
+  contentSecurityPolicy: isProduction
+    ? {
+        useDefaults: true,
+        directives: {
+          defaultSrc: ["'self'"],
+        },
+      }
+    : false,
+  crossOriginEmbedderPolicy: false,
+  referrerPolicy: { policy: 'no-referrer' },
+});
