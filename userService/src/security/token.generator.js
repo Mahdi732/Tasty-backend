edc72477e3615b3f4d763691738d42c94d088d2e
Mahdi732 <ey449751@gmail.com>
2026-02-25 11:04:35 +0000

    feat: implement TokenGenerator class for session and refresh token generation

---
+++ userService/src/security/token.generator.js
@@ -0,0 +1,20 @@
+import { v4 as uuidv4 } from 'uuid';
+import { randomUrlSafe } from './crypto.utils.js';
+
+export class TokenGenerator {
+  createSessionId() {
+    return uuidv4();
+  }
+
+  createFamilyId() {
+    return uuidv4();
+  }
+
+  createRefreshSecret() {
+    return randomUrlSafe(48);
+  }
+
+  buildRefreshToken(sessionId, secret) {
+    return `${sessionId}.${secret}`;
+  }
+}
