88dc8a9be4c35059e8e32db1034e153c57a04a9c
Mahdi732 <ey449751@gmail.com>
2026-02-25 11:03:18 +0000

    feat: implement GoogleOAuthProvider for Google OAuth integration

---
+++ userService/src/oauth/providers/google.provider.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import { OAuthProvider } from '../oauth-provider.interface.js';
+
+export class GoogleOAuthProvider extends OAuthProvider {
+  constructor(env) {
+    super();
+    this.clientId = env.GOOGLE_CLIENT_ID;
+    this.clientSecret = env.GOOGLE_CLIENT_SECRET;
+    this.redirectUri = env.GOOGLE_REDIRECT_URI;
+  }
+
+  getName() {
+    return 'google';
+  }
+
+  getAuthorizationUrl(state, codeChallenge) {
+    const url = new URL('https://accounts.google.com/o/oauth2/v2/auth');
+    url.searchParams.set('client_id', this.clientId);
+    url.searchParams.set('redirect_uri', this.redirectUri);
+    url.searchParams.set('response_type', 'code');
+    url.searchParams.set('scope', 'openid email profile');
+    url.searchParams.set('state', state);
+    if (codeChallenge) {
+      url.searchParams.set('code_challenge', codeChallenge);
+      url.searchParams.set('code_challenge_method', 'S256');
+    }
+    return url.toString();
+  }
+
+  async exchangeCodeForProfile(code, codeVerifier) {
+    const tokenResponse = await axios.post(
+      'https://oauth2.googleapis.com/token',
+      new URLSearchParams({
+        client_id: this.clientId,
+        client_secret: this.clientSecret,
+        code,
+        grant_type: 'authorization_code',
+        redirect_uri: this.redirectUri,
+        code_verifier: codeVerifier || '',
+      }),
+      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+    );
+
+    const accessToken = tokenResponse.data.access_token;
+
+    const userInfoResponse = await axios.get('https://openidconnect.googleapis.com/v1/userinfo', {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+
+    return {
+      provider: 'google',
+      providerUserId: userInfoResponse.data.sub,
+      email: userInfoResponse.data.email,
+      emailVerified: Boolean(userInfoResponse.data.email_verified),
+      name: userInfoResponse.data.name || null,
+      profile: userInfoResponse.data,
+    };
+  }
+}
