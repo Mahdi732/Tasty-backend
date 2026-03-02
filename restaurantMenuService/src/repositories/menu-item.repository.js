04bf9a3750034410c0f6822ef15e7aeb3fe4f353
Mahdi732 <ey449751@gmail.com>
2026-03-02 15:40:38 +0000

    feat: add MenuItemRepository for managing menu items

---
+++ restaurantMenuService/src/repositories/menu-item.repository.js
@@ -0,0 +1,44 @@
+export class MenuItemRepository {
+  constructor(model) {
+    this.model = model;
+  }
+
+  create(payload) {
+    return this.model.create(payload);
+  }
+
+  listByRestaurant(restaurantId) {
+    return this.model
+      .find({ restaurantId, deletedAt: null })
+      .sort({ sortOrder: 1 })
+      .lean();
+  }
+
+  listPublishedByRestaurant(restaurantId, includeOutOfStock = false) {
+    const filter = {
+      restaurantId,
+      deletedAt: null,
+      isPublished: true,
+    };
+    if (!includeOutOfStock) {
+      filter.availability = 'IN_STOCK';
+    }
+
+    return this.model.find(filter).sort({ sortOrder: 1 }).lean();
+  }
+
+  findById(id) {
+    return this.model.findOne({ _id: id, deletedAt: null });
+  }
+
+  updateById(id, payload) {
+    return this.model.findOneAndUpdate({ _id: id, deletedAt: null }, payload, {
+      new: true,
+      runValidators: true,
+    });
+  }
+
+  softDelete(id) {
+    return this.updateById(id, { deletedAt: new Date() });
+  }
+}
