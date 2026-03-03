46895e1ccafbbf3e1968831a1d3cecd036694188
Mahdi732 <ey449751@gmail.com>
2026-03-03 09:39:41 +0000

    feat: add meta parameter to ApiError constructor for enhanced error context

--- userService/src/utils/api-error.js
+++ userService/src/utils/api-error.js
@@ -1,8 +1,9 @@
 export class ApiError extends Error {
-  constructor(statusCode, code, message, details) {
+  constructor(statusCode, code, message, details, meta) {
     super(message);
     this.statusCode = statusCode;
     this.code = code;
     this.details = details;
+    this.meta = meta;
   }
 }
