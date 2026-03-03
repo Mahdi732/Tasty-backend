e854412dbb01a54ed30c1822959ede6ede04a844
Mahdi732 <ey449751@gmail.com>
2026-03-03 09:40:04 +0000

    feat: enhance createTestContext to include otpCode and MemoryEmailSender

--- userService/tests/helpers/test-app.js
+++ userService/tests/helpers/test-app.js
@@ -1,13 +1,21 @@
 import request from 'supertest';
 import { buildApp } from '../../src/app.js';
 import { RedisMock } from './redis-mock.js';
+import { MemoryEmailSender } from './memory-email.sender.js';
 
-export const createTestContext = async () => {
+export const createTestContext = async ({ otpCode = '123456' } = {}) => {
   const redis = new RedisMock();
-  const app = await buildApp({ redisClient: redis });
+  const emailSender = new MemoryEmailSender();
+  const app = await buildApp({
+    redisClient: redis,
+    otpGenerator: () => otpCode,
+    emailSender,
+  });
   return {
     app,
     redis,
+    emailSender,
+    otpCode,
     request: request(app),
   };
 };
