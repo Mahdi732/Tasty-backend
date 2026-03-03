14bda1da33c8ce1930e69b844b02bca01dcfcb12
Mahdi732 <ey449751@gmail.com>
2026-03-03 09:39:37 +0000

    feat: add NoopEmailSender implementation for email verification OTPs

---
+++ userService/src/services/email/noop-email.sender.js
@@ -0,0 +1,7 @@
+import { EmailSender } from './email-sender.interface.js';
+
+export class NoopEmailSender extends EmailSender {
+  async sendVerificationOtp() {
+    return { accepted: true };
+  }
+}
