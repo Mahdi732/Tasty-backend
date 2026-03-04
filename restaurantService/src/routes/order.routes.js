144de87c6c5d90ee94d039371e3467bf61af21cf
Mahdi732 <ey449751@gmail.com>
2026-03-04 14:52:26 +0000

    chore: add manager and public routes with CRUD operations for restaurants and menu items

---
+++ restaurantService/src/routes/order.routes.js
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import { asyncHandler } from '../utils/async-handler.js';
+import { requireRole } from '../middlewares/role.middleware.js';
+import { validate } from '../middlewares/validate.middleware.js';
+import { ROLES } from '../constants/roles.js';
+import { createOrderSchema, restaurantOrdersParamSchema } from '../validators/order.validator.js';
+
+export const buildOrderRoutes = ({ orderController, requireRestaurantOrderAccess }) => {
+  const router = Router();
+
+  router.post(
+    '/orders',
+    requireRole(ROLES.USER, ROLES.MANAGER, ROLES.STAFF, ROLES.WORKER, ROLES.SUPERADMIN),
+    validate(createOrderSchema),
+    asyncHandler(orderController.placeOrder)
+  );
+
+  router.get('/orders/me', asyncHandler(orderController.listMyOrders));
+
+  router.get(
+    '/orders/restaurant/:restaurantId',
+    requireRole(ROLES.MANAGER, ROLES.STAFF, ROLES.WORKER, ROLES.SUPERADMIN),
+    validate(restaurantOrdersParamSchema, 'params'),
+    requireRestaurantOrderAccess,
+    asyncHandler(orderController.listRestaurantOrders)
+  );
+
+  router.get('/admin/orders', requireRole(ROLES.SUPERADMIN), asyncHandler(orderController.listAll));
+
+  return router;
+};
---
+++ restaurantService/src/routes/public.routes.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { validate } from '../middlewares/validate.middleware.js';
+import { asyncHandler } from '../utils/async-handler.js';
+import { citySlugParamSchema, paginationQuerySchema } from '../validators/restaurant.validator.js';
+
+export const buildPublicRoutes = (publicController) => {
+  const router = Router();
+
+  router.get('/restaurants', validate(paginationQuerySchema, 'query'), asyncHandler(publicController.listRestaurants));
+  router.get(
+    '/restaurants/:citySlug/:slug',
+    validate(citySlugParamSchema, 'params'),
+    asyncHandler(publicController.getRestaurant)
+  );
+  router.get(
+    '/restaurants/:citySlug/:slug/menu',
+    validate(citySlugParamSchema, 'params'),
+    validate(paginationQuerySchema, 'query'),
+    asyncHandler(publicController.getMenu)
+  );
+
+  return router;
+};
