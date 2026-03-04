33f06ca389aee39c219cfbbcf268aa67eebfb511
Mahdi732 <ey449751@gmail.com>
2026-03-04 10:27:20 +0000

    test: update manager scoping tests to reflect restaurant management capabilities

--- restaurantMenuService/tests/integration/manager-scoping.test.js
+++ restaurantMenuService/tests/integration/manager-scoping.test.js
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import { setupTestApp, teardownTestApp, resetDatabase } from '../helpers/test-app.js';
 
-describe('Manager tenant scoping', () => {
+describe('Manager restaurant scoping', () => {
   let app;
 
   beforeAll(async () => {
@@ -38,5 +38,41 @@ describe('Manager tenant scoping', () => {
 
     expect(updateRes.body.success).toBe(false);
     expect(updateRes.body.error.code).toBe('TENANT_ACCESS_DENIED');
+  });
+
+  it('same manager can manage multiple restaurants', async () => {
+    const createA = await request(app)
+      .post('/restaurants')
+      .set('x-test-user-id', 'manager-a')
+      .set('x-test-roles', 'manager')
+      .send({
+        name: 'Manager A Restaurant 1',
+        location: { city: 'Rabat', citySlug: 'rabat' },
+      })
+      .expect(201);
+
+    const createB = await request(app)
+      .post('/restaurants')
+      .set('x-test-user-id', 'manager-a')
+      .set('x-test-roles', 'manager')
+      .send({
+        name: 'Manager A Restaurant 2',
+        location: { city: 'Rabat', citySlug: 'rabat' },
+      })
+      .expect(201);
+
+    await request(app)
+      .patch(`/restaurants/${createA.body.data._id}`)
+      .set('x-test-user-id', 'manager-a')
+      .set('x-test-roles', 'manager')
+      .send({ description: 'updated-a' })
+      .expect(200);
+
+    await request(app)
+      .patch(`/restaurants/${createB.body.data._id}`)
+      .set('x-test-user-id', 'manager-a')
+      .set('x-test-roles', 'manager')
+      .send({ description: 'updated-b' })
+      .expect(200);
   });
 });
