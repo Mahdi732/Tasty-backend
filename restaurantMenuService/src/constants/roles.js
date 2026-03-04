d4b024136668b42465e20cb94c817adcfe1fc7b3
Mahdi732 <ey449751@gmail.com>
2026-03-04 14:44:34 +0000

    chore: remove unused configuration, constants, and controller files

--- restaurantMenuService/src/constants/roles.js
+++
@@ -1,9 +0,0 @@
-export const ROLES = Object.freeze({
-  USER: 'user',
-  WORKER: 'worker',
-  STAFF: 'staff',
-  MANAGER: 'manager',
-  SUPERADMIN: 'superadmin',
-});
-
-export const ROLE_LIST = Object.values(ROLES);
