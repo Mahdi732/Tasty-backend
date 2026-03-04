30e20c696d5f663e783f07347f627eebc0ad3025
Mahdi732 <ey449751@gmail.com>
2026-03-04 14:47:44 +0000

    chore: implement OrderController for managing orders

---
+++ restaurantService/src/controllers/order.controller.js
@@ -0,0 +1,27 @@
+import { ok } from '../utils/api-response.js';
+
+export class OrderController {
+  constructor(orderService) {
+    this.orderService = orderService;
+  }
+
+  placeOrder = async (req, res) => {
+    const order = await this.orderService.placeOrder(req.auth, req.body);
+    return ok(res, order, 201);
+  };
+
+  listMyOrders = async (req, res) => {
+    const orders = await this.orderService.listMyOrders(req.auth);
+    return ok(res, orders);
+  };
+
+  listRestaurantOrders = async (req, res) => {
+    const orders = await this.orderService.listRestaurantOrders(req.params.restaurantId, req.auth);
+    return ok(res, orders);
+  };
+
+  listAll = async (_req, res) => {
+    const orders = await this.orderService.listAllOrders();
+    return ok(res, orders);
+  };
+}
