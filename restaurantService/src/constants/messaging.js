852ef3d9096af101742197ccf06b6bd77fc04517
Mahdi732 <ey449751@gmail.com>
2026-03-04 14:47:06 +0000

    chore: add messaging constants for restaurant events

---
+++ restaurantService/src/constants/messaging.js
@@ -0,0 +1,5 @@
+export const EVENTS = Object.freeze({
+  RESTAURANT_CREATED: 'restaurant.created',
+  RESTAURANT_STAFF_ASSIGNED: 'restaurant.staff.assigned',
+  RESTAURANT_STAFF_REMOVED: 'restaurant.staff.removed',
+});
