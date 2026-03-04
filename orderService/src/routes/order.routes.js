cfca0b63549c8167e30c28704827289c9aee8ab6
Mahdi732 <ey449751@gmail.com>
2026-03-04 14:26:35 +0000

    feat: add order routes for managing user and restaurant orders

---
+++ orderService/src/routes/order.routes.js
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import { asyncHandler } from '../utils/async-handler.js';
+import { validate } from '../middlewares/validate.middleware.js';
+import { createOrderSchema, restaurantOrdersParamSchema, scanQrSchema } from '../validators/order.validator.js';
+import { ROLES } from '../constants/roles.js';
+
+export const buildOrderRoutes = ({
+  orderController,
+  authMiddleware,
+  requireRole,
+  requireRestaurantAccess,
+}) => {
+  const router = Router();
+
+  router.use(authMiddleware);
+
+  router.post('/me', validate(createOrderSchema), asyncHandler(orderController.create));
+  router.get('/me', asyncHandler(orderController.myOrders));
+
+  router.get(
+    '/restaurant/:restaurantId',
+    validate(restaurantOrdersParamSchema, 'params'),
+    requireRole(ROLES.WORKER, ROLES.STAFF, ROLES.MANAGER, ROLES.SUPERADMIN),
+    requireRestaurantAccess,
+    asyncHandler(orderController.restaurantOrders)
+  );
+
+  router.get('/admin/all', requireRole(ROLES.SUPERADMIN), asyncHandler(orderController.listAll));
+
+  router.post(
+    '/qr/scan',
+    validate(scanQrSchema),
+    requireRole(ROLES.WORKER, ROLES.STAFF, ROLES.MANAGER, ROLES.SUPERADMIN),
+    asyncHandler(orderController.scanQr)
+  );
+
+  return router;
+};
