d70c5b80581d84ee5e98a8411c634a50cf518663
Mahdi732 <ey449751@gmail.com>
2026-03-04 14:49:40 +0000

    chore: add validate middleware for request body validation

---
+++ restaurantService/src/middlewares/validate.middleware.js
@@ -0,0 +1,13 @@
+import { ApiError } from '../utils/api-error.js';
+import { ERROR_CODES } from '../constants/errors.js';
+
+export const validate = (schema, source = 'body') => (req, _res, next) => {
+  const result = schema.safeParse(req[source] || {});
+  if (!result.success) {
+    return next(
+      new ApiError(400, ERROR_CODES.VALIDATION_ERROR, 'Validation error', result.error.flatten())
+    );
+  }
+  req[source] = result.data;
+  return next();
+};
