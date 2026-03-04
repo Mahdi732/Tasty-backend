b2fc687264a866ab76d7bd43e1f7e2fbe6fc7633
Mahdi732 <ey449751@gmail.com>
2026-03-04 14:24:08 +0000

    feat: implement OrderController with CRUD operations for orders

---
+++ orderService/src/controllers/order.controller.js
@@ -0,0 +1,33 @@
+import { ok } from '../utils/api-response.js';
+import { buildEventHeaders } from '../utils/request-context.js';
+
+export class OrderController {
+  constructor(orderService) {
+    this.orderService = orderService;
+  }
+
+  create = async (req, res) => {
+    const order = await this.orderService.createOrder(req.auth, req.body, buildEventHeaders(req));
+    return ok(res, order, 201);
+  };
+
+  myOrders = async (req, res) => {
+    const orders = await this.orderService.listMyOrders(req.auth);
+    return ok(res, orders);
+  };
+
+  restaurantOrders = async (req, res) => {
+    const orders = await this.orderService.listRestaurantOrders(req.params.restaurantId, req.auth);
+    return ok(res, orders);
+  };
+
+  listAll = async (_req, res) => {
+    const orders = await this.orderService.listAllOrders();
+    return ok(res, orders);
+  };
+
+  scanQr = async (req, res) => {
+    const updated = await this.orderService.scanQr(req.body.qrToken, req.auth.userId, buildEventHeaders(req));
+    return ok(res, updated);
+  };
+}
