c64cdedea075d984216b31a9c80281e8a0aaea92
Mahdi732 <ey449751@gmail.com>
2026-03-04 14:23:36 +0000

    feat: implement payment consumer for processing payment succeeded events

---
+++ orderService/src/consumers/payment.consumer.js
@@ -0,0 +1,12 @@
+import { EVENTS } from '../constants/messaging.js';
+
+export const registerPaymentConsumer = async ({ rabbitBus, orderService, queueName, logger }) => {
+  await rabbitBus.subscribe({
+    queue: queueName,
+    routingKeys: [EVENTS.PAYMENT_SUCCEEDED],
+    onMessage: async (payload, headers) => {
+      await orderService.onPaymentSucceeded(payload, headers);
+      logger.info({ event: EVENTS.PAYMENT_SUCCEEDED, resourceId: payload.resourceId }, 'payment_event_processed');
+    },
+  });
+};
