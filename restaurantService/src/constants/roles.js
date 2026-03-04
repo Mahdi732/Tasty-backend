345bac9ad02ea156db6d3c92762ccef1abb5e10a
Mahdi732 <ey449751@gmail.com>
2026-03-04 14:47:21 +0000

    chore: add roles and role list constants for user management

---
+++ restaurantService/src/constants/roles.js
@@ -0,0 +1,9 @@
+export const ROLES = Object.freeze({
+  USER: 'user',
+  WORKER: 'worker',
+  STAFF: 'staff',
+  MANAGER: 'manager',
+  SUPERADMIN: 'superadmin',
+});
+
+export const ROLE_LIST = Object.values(ROLES);
