7e9e7aa5bca030f61e14fe9790f80c22e77f172a
Mahdi732 <ey449751@gmail.com>
2026-03-08 17:34:13 +0000

    chore: add CHEF and DELIVERY_MAN roles to the roles constant

--- orderService/src/constants/roles.js
+++ orderService/src/constants/roles.js
@@ -2,6 +2,8 @@ export const ROLES = Object.freeze({
   USER: 'user',
   WORKER: 'worker',
   STAFF: 'staff',
+  CHEF: 'chef',
+  DELIVERY_MAN: 'delivery_man',
   MANAGER: 'manager',
   SUPERADMIN: 'superadmin',
 });
