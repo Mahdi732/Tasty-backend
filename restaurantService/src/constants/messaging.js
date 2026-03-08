ce65b2d0c260b7f2bd50f08fab21e7d655f72f6d
Mahdi732 <ey449751@gmail.com>
2026-03-08 17:35:31 +0000

    add new event constants for restaurant management and inventory checks

--- restaurantService/src/constants/messaging.js
+++ restaurantService/src/constants/messaging.js
@@ -2,4 +2,7 @@ export const EVENTS = Object.freeze({
   RESTAURANT_CREATED: 'restaurant.created',
   RESTAURANT_STAFF_ASSIGNED: 'restaurant.staff.assigned',
   RESTAURANT_STAFF_REMOVED: 'restaurant.staff.removed',
+  RESTAURANT_ARCHIVED: 'restaurant.archived',
+  RESTORE_FEE_PAYMENT_REQUESTED: 'restaurant.restore.fee.payment.requested',
+  INVENTORY_CHECK: 'inventory.check',
 });
