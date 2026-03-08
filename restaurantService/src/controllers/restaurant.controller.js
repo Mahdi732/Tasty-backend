af11800038814a46b237e5bf33e73baf2a2dceb8
Mahdi732 <ey449751@gmail.com>
2026-03-08 17:36:07 +0000

    reorganize restaurant controller methods for better structure

--- restaurantService/src/controllers/restaurant.controller.js
+++ restaurantService/src/controllers/restaurant.controller.js
@@ -29,4 +29,19 @@ export class RestaurantController {
     const mapping = await this.restaurantService.addStaffMember(req.params.id, req.auth, req.body);
     return ok(res, mapping, 201);
   };
+
+  archive = async (req, res) => {
+    const restaurant = await this.restaurantService.archiveRestaurant(req.params.id, req.auth);
+    return ok(res, restaurant);
+  };
+
+  requestRestore = async (req, res) => {
+    const restaurant = await this.restaurantService.requestRestoreFee(req.params.id, req.auth, req.body);
+    return ok(res, restaurant);
+  };
+
+  triggerLowStockAlert = async (req, res) => {
+    const result = await this.restaurantService.triggerLowStockAlert(req.params.id, req.auth, req.body);
+    return ok(res, result, 202);
+  };
 }
