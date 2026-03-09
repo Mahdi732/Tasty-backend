b5c138df87da46afc1ab8aba80d53255bc9d52a2
Mahdi732 <ey449751@gmail.com>
2026-03-09 15:38:11 +0000

    feat: implement server setup for face recognition service

---
+++ faceRecognitionService/src/server.js
@@ -0,0 +1,31 @@
+import { buildApp } from './app.js';
+import { connectMongo, env, logger } from './config/index.js';
+
+let server;
+
+const start = async () => {
+  await connectMongo(env.MONGO_URI);
+
+  const app = await buildApp();
+  server = app.listen(env.PORT, () => {
+    logger.info({ port: env.PORT }, 'face_recognition_service_started');
+  });
+};
+
+const shutdown = async (signal) => {
+  logger.info({ signal }, 'shutting_down');
+
+  if (server) {
+    await new Promise((resolve) => server.close(resolve));
+  }
+
+  process.exit(0);
+};
+
+process.on('SIGTERM', () => shutdown('SIGTERM'));
+process.on('SIGINT', () => shutdown('SIGINT'));
+
+start().catch((error) => {
+  logger.error({ err: error }, 'startup_failed');
+  process.exit(1);
+});
