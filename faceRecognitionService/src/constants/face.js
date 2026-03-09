4bc90a91c4dae36403e1cdfe40f47982904962f1
Mahdi732 <ey449751@gmail.com>
2026-03-09 15:39:13 +0000

    feat: add constants for list types and match decisions

---
+++ faceRecognitionService/src/constants/face.js
@@ -0,0 +1,11 @@
+export const LIST_TYPE = Object.freeze({
+  NORMAL: 'NORMAL',
+  BANNED: 'BANNED',
+  DEBTOR: 'DEBTOR',
+});
+
+export const MATCH_DECISION = Object.freeze({
+  NO_MATCH: 'NO_MATCH',
+  REVIEW: 'REVIEW',
+  MATCH_BLOCK: 'MATCH_BLOCK',
+});
