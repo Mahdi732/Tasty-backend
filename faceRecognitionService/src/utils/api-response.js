4a88e738e662419c830f0f0a40d72d0fddb4ad6d
Mahdi732 <ey449751@gmail.com>
2026-03-10 11:40:28 +0000

    feat: add utility functions for standardized API responses

---
+++ faceRecognitionService/src/utils/api-response.js
@@ -0,0 +1,5 @@
+export const ok = (res, data, statusCode = 200, meta) =>
+  res.status(statusCode).json({ success: true, data, meta });
+
+export const fail = (res, statusCode, error) =>
+  res.status(statusCode).json({ success: false, error });
