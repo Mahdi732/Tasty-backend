0ed980c57abf5cc627906b7246ab8ebb65a70026
Mahdi732 <ey449751@gmail.com>
2026-03-10 11:43:14 +0000

    feat: refactor validate middleware to utilize createValidateMiddleware for improved validation handling

--- orderService/src/middlewares/validate.middleware.js
+++ orderService/src/middlewares/validate.middleware.js
@@ -1,11 +1,8 @@
 import { ApiError } from '../utils/api-error.js';
 import { ERROR_CODES } from '../constants/errors.js';
+import { createValidateMiddleware } from '../../../../common/src/middlewares/validate.middleware.js';
 
-export const validate = (schema, source = 'body') => (req, _res, next) => {
-  const result = schema.safeParse(req[source] || {});
-  if (!result.success) {
-    return next(new ApiError(400, ERROR_CODES.VALIDATION_ERROR, 'Validation error', result.error.flatten()));
-  }
-  req[source] = result.data;
-  return next();
-};
+export const validate = createValidateMiddleware({
+  ApiError,
+  validationErrorCode: ERROR_CODES.VALIDATION_ERROR,
+});
