a756fa7db5aa989f54ac682bf3eba50e6da13d3d
Mahdi732 <ey449751@gmail.com>
2026-03-17 10:06:19 +0000

    feat: add not found middleware for handling 404 errors

---
+++ paymentService/src/middlewares/not-found.middleware.js
@@ -0,0 +1,6 @@
+import { ApiError } from '../utils/api-error.js';
+import { ERROR_CODES } from '../constants/errors.js';
+
+export const notFoundMiddleware = (_req, _res, next) => {
+  next(new ApiError(404, ERROR_CODES.NOT_FOUND, 'Route not found'));
+};
