316f4e8f8bd3dcab5e858b20ba8526bcf3b2974d
Mahdi732 <ey449751@gmail.com>
2026-03-17 10:07:26 +0000

    feat: add validation middleware for handling request validation

---
+++ paymentService/src/middlewares/validate.middleware.js
@@ -0,0 +1,8 @@
+import { ApiError } from '../utils/api-error.js';
+import { ERROR_CODES } from '../constants/errors.js';
+import { createValidateMiddleware } from '../../../common/src/middlewares/validate.middleware.js';
+
+export const validate = createValidateMiddleware({
+  ApiError,
+  validationErrorCode: ERROR_CODES.VALIDATION_ERROR,
+});
