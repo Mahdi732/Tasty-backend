2ded00952e445a329778921cee77ed636448d901
Mahdi732 <ey449751@gmail.com>
2026-03-17 10:05:14 +0000

    feat: add payment transaction types and statuses constants

---
+++ paymentService/src/constants/payment.js
@@ -0,0 +1,10 @@
+export const PAYMENT_TRANSACTION_TYPE = Object.freeze({
+  SUBSCRIPTION: 'SUBSCRIPTION',
+  ORDER: 'ORDER',
+});
+
+export const PAYMENT_STATUS = Object.freeze({
+  PENDING: 'PENDING',
+  SUCCESS: 'SUCCESS',
+  FAILED: 'FAILED',
+});
