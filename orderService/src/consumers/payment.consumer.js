52826ddefe92c6ab0d864eb51464b8b067ad9a86
Mahdi732 <ey449751@gmail.com>
2026-03-17 10:03:54 +0000

    feat: add support for PAYMENT_ORDER_SUCCESS event in payment consumer

--- orderService/src/consumers/payment.consumer.js
+++ orderService/src/consumers/payment.consumer.js
@@ -3,8 +3,14 @@ import { EVENTS } from '../constants/messaging.js';
 export const registerPaymentConsumer = async ({ rabbitBus, orderService, queueName, logger }) => {
   await rabbitBus.subscribe({
     queue: queueName,
-    routingKeys: [EVENTS.PAYMENT_SUCCEEDED],
+    routingKeys: [EVENTS.PAYMENT_SUCCEEDED, EVENTS.PAYMENT_ORDER_SUCCESS],
     onMessage: async (payload, headers) => {
+      if (headers.routingKey === EVENTS.PAYMENT_ORDER_SUCCESS) {
+        await orderService.onOrderPaymentSucceeded(payload, headers);
+        logger.info({ event: EVENTS.PAYMENT_ORDER_SUCCESS, orderId: payload.orderId }, 'payment_event_processed');
+        return;
+      }
+
       await orderService.onPaymentSucceeded(payload, headers);
       logger.info({ event: EVENTS.PAYMENT_SUCCEEDED, resourceId: payload.resourceId }, 'payment_event_processed');
     },
