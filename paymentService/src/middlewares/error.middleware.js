8880a079e69136c040289e44a6b3b57f85776c1c
Mahdi732 <ey449751@gmail.com>
2026-03-17 10:06:12 +0000

    feat: add error middleware for centralized error handling

---
+++ paymentService/src/middlewares/error.middleware.js
@@ -0,0 +1,11 @@
+import { ApiError } from '../utils/api-error.js';
+import { fail } from '../utils/api-response.js';
+import { ERROR_CODES } from '../constants/errors.js';
+import { createErrorMiddleware } from '../../../common/src/middlewares/error.middleware.js';
+
+export const errorMiddleware = (logger) => createErrorMiddleware({
+  logger,
+  ApiError,
+  fail,
+  internalErrorCode: ERROR_CODES.INTERNAL_ERROR,
+});
